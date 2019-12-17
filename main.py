f79ba14a51c04b2c08916f9ea974e491da685077
Skywal <skaywoker6@gmail.com>
2019-12-17 21:06:54 +0200

    Small refactor

--- main.py
+++ main.py
@@ -16,10 +16,26 @@ class PerceptronApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         super().__init__()
         self.setupUi(self) # this needed for initializing of design
         
-        # bind events and event holders
-        self.selectButton.clicked.connect(self.browse_folder_action) 
-        self.loadButton.clicked.connect(self.load_data_action)
-        self.StartButton.clicked.connect(self.start_action)
+        self.setup_vars() # make instance of all global variables for this class
+
+        self.start_conditions() # apply firs start condition 
+
+        self.event_holders() # bind events and event holders
+
+        self.setup_graph() # implementing graph output 
+    
+    def setup_vars(self):
+        """ Initial setup of the global variables """
+        
+        self.maxNumEpoch = 1000 # maximum number of epochs that will be perform at training model
+        self.maxError = 0 # maximal possible error of data evaluation
+        self.passedEpochs = 0 # epoch count that passed before successfull training
+        self.error = 0 # actual error of data evaluation
+
+        self.database = db.Database() # create instance of database object to work with .CSV file and data
+    
+    def setup_graph(self):
+        """ Initial setup of the graphical output """
 
         self.graph_widg = graph.Graph() # create instance of the graph class
         self.graph_layout = QtWidgets.QVBoxLayout(self.graphic) # creating layout inside an empty widget
@@ -28,7 +44,16 @@ class PerceptronApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
         self.graph_layout.addWidget(self.graph_widg) # add graph widget insige layout
 
-        self.database = db.Database() # create instance of database object to work with .CSV file and data
+    def start_conditions(self):
+        self.maxNumEpochInput.setText(str(self.maxNumEpoch))
+        self.maxErrorInput.setText(str(self.maxError))
+
+    def event_holders(self):
+        """ Bind events """
+
+        self.selectButton.clicked.connect(self.browse_folder_action) 
+        self.loadButton.clicked.connect(self.load_data_action)
+        self.StartButton.clicked.connect(self.start_action)
 
     def browse_folder_action(self):
         """Select file from local disc by button press"""
